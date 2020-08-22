ba77a62dcb46d3a084cf9e1c5d54e66da49b7240
Raul Alzate <raulest50@gmail.com>
2020-08-21 21:56:04 -0500

    Hice correcciones importantes en la actividad de ParamAct.java que se usa ahora para hacer deteccion automatica de la direccion ip del servidor. se agrego un nuevo hilo para que haga un timeout al proceso de udp broadcasting and listening. Se organizo mas el codigo y ahora esta todo contenido en la clase SatelinkFinder, solo de debe llamar a Scan(). Ahora la deteccion funciona mucho mejor en el caso que el servidor no esta en linea y cuando si esta en linea la deteccioon sigue igual de rapida y efectiva, con esto se logra un comportamiento mas consistente.
    featured added: deteccion automatica IP de servidor - TERMINADO

--- app/src/main/java/com/rendidor/irsum/ParamAct.java
+++ app/src/main/java/com/rendidor/irsum/ParamAct.java
@@ -1,15 +1,17 @@
 package com.rendidor.irsum;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,37 +24,49 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+// explicacion de completable future
+// https://levelup.gitconnected.com/completablefuture-a-new-era-of-asynchronous-programming-86c2fe23e246
 
 public class ParamAct extends SuperAct {
     Editor edit;
     Button Button_Buscar_Satelink;
     SharedPreferences pre;
-    EditText EditText_Host;
+
     Button Button_volver;
     TextView TXView_show_serverIP; // etiqueta para mostrar la direccion ip del servidor.
 
-    public SatelinkFinder sf; // se usa para encontrar la direccion ip del server con udp broadcast
+    public final String BLANK_IP_LABEL = "--- . --- . --- . ---";
+
+    public SatelinkFinder sf = new SatelinkFinder(this); // se usa para encontrar la direccion ip del server con udp broadcast
 
     /* access modifiers changed from: protected */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_param);
+
+        // Instacias elementos UI
         this.Button_volver = findViewById(R.id.Button_volver);
         this.Button_Buscar_Satelink = findViewById(R.id.Button_Buscar_Satelink);
-        this.EditText_Host = findViewById(R.id.Tx_Direccion);
-        this.pre = getSharedPreferences("configuracion_irsum", 0);
-        this.TXView_show_serverIP = findViewById(R.id.TXView_show_serverIP);
-        this.edit = this.pre.edit();
-        this.EditText_Host.setText(this.pre.getString("host", ""));
 
+        this.TXView_show_serverIP = findViewById(R.id.TXView_show_serverIP);
+
+        // java preference api
+        this.pre = getSharedPreferences("configuracion_irsum", 0);
+        this.edit = this.pre.edit();
+        this.TXView_show_serverIP.setText(this.pre.getString("host", BLANK_IP_LABEL));
+        //this.EditText_Host.setText(this.pre.getString("host", ""));
+
+        // Button Listeners
         this.Button_Buscar_Satelink.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                /*
-                edit.putString("host", ParamAct.this.EditText_Host.getText().toString());
-                edit.apply();
-                */
                 BuscarSatelink();
-                ShowT("buscando satelink server...");
             }
         });
 
@@ -74,39 +88,94 @@ public class ParamAct extends SuperAct {
 
 
     public void BuscarSatelink() {
-        sf = new SatelinkFinder();
-        //try {sf.reestablecer();} catch (NullPointerException e){ sf = new SatelinkFinder(); }
-        if (!sf.running) {
-            sf.start();
-            Toast toast = Toast.makeText(getApplicationContext(), "Buscando servidor satelink ...", Toast.LENGTH_SHORT);
-            toast.show();
-        }
+        sf.Scan();
     }
 
     /**
      * La clase satelinkFinder contiene lo necesario para hacer una deteccion de la direccion
      * ip de satelink (el servidor de nodejs) usando udp broadcasting
      */
-    public class SatelinkFinder extends Thread {
+    public class SatelinkFinder{
+        // --- VARIABLES
         DatagramSocket socket; // para enviar y recivir informacion
         boolean running; // para indicar si el hilo esta o no corriendo
         byte[] buf = new byte[31];
-        // String que se envia a satelink por medio de udp para indicarle que envie su direccion ip
-        final byte[] SATELINK_IP_COMMAND = "satelink.ip".getBytes();
-        InetAddress BROADCAST_IP; // direccion local de broadcast, depende de la subnet
+        InetAddress broadcastIP; // direccion local de broadcast, depende de la subnet
 
         // para definir en que fase de la comunicacion se encuentra, como una maquina de estados
         // 0 > hacer broadcast. 1 > esperar ip del servidor. 2 > ip recivida fin del hilo
-        public int estado = 0;
+        public int estado;
+
+        Context contexto;
+
+        // --- CONSTANTES
+        // tiempo de espera en milisegundos para escuchar respuesta del servidor
+        public int TIME_OUT_SEC = 7;
+
+        // byte String que se envia a satelink por medio de udp para indicarle que envie su direccion ip
+        final byte[] SATELINK_IP_COMMAND = "satelink.ip".getBytes();
 
 
-        public SatelinkFinder() {
+        public class SFTimer extends Thread {
+            public Thread udp_thread;
+            @Override
+            public void run() {
+                try {
+                    sleep(7000);
+                    udp_thread.interrupt();
+                } catch (InterruptedException e) {
+                    //e.printStackTrace();
+                }
+            }
+        }
+
+        //CONSTRUCTOR
+        public SatelinkFinder(Context contexto) {
             try {
-                socket = new DatagramSocket(12100);
+                this.socket = new DatagramSocket(12100);
+                this.running = false;
+                this.estado = 0;
+                this.contexto = contexto;
                 // se determina la direccion ip de broadcast
-                this.BROADCAST_IP = GetBroadcast();
+                this.broadcastIP = GetBroadcast();
             } catch (SocketException | UnknownHostException e) {
                 e.printStackTrace();
+            }
+        }
+
+        // Executors.newSingleThreadExecutor().execute(() -> {});
+
+        /**
+         *
+         */
+        public void Scan(){
+            if(!running) { // prevent more than one thread.
+                Toast toast = Toast.makeText(getApplicationContext(), "Buscando servidor...", Toast.LENGTH_SHORT);
+                toast.show();
+                Thread udp_thread = new Thread(){
+                    @Override
+                    public void run() {
+                        SFTimer tmout = new SFTimer();
+                        tmout.udp_thread = this;
+                        tmout.start();
+                        String ip = UDP_Broadcast();
+                        tmout.interrupt();
+                        DoWhenComplete(ip);
+                        reset();
+                    }
+
+                    @Override
+                    public void interrupt() {
+                        DoWhenComplete(BLANK_IP_LABEL);
+                        //Toast toast2 = Toast.makeText(contexto, "No se encontro el servidor", Toast.LENGTH_SHORT);
+                        //toast2.show();
+                        super.interrupt();
+                    }
+                };
+                udp_thread.start();
+            } else{
+                Toast toast = Toast.makeText(getApplicationContext(), "Ya se inicio la busqueda del servidor", Toast.LENGTH_SHORT);
+                toast.show();
             }
         }
 
@@ -132,11 +201,12 @@ public class ParamAct extends SuperAct {
             return broadcast;
         }
 
-        public void run() {
+        public String UDP_Broadcast(){
             running = true;
+            String r = "";
             try {
                 DatagramPacket data_send = new DatagramPacket( // satelink siempre escucha datagramas udp en el puerto 2100
-                        this.SATELINK_IP_COMMAND, this.SATELINK_IP_COMMAND.length, this.BROADCAST_IP, 2100);
+                        this.SATELINK_IP_COMMAND, this.SATELINK_IP_COMMAND.length, this.broadcastIP, 2100);
                 socket.send(data_send);
                 this.estado = 1; // se pasa a la fase de escucha
                 while (estado == 1) {
@@ -146,49 +216,56 @@ public class ParamAct extends SuperAct {
                     InetAddress address = packet.getAddress();
                     int port = packet.getPort();
                     packet = new DatagramPacket(buf, buf.length, address, port);
-                    String received = new String(packet.getData(), packet.getOffset(), packet.getLength());
-                    //Log.i("**********", received);
+                    String received = new String(packet.getData(), packet.getOffset(), packet.getLength()); // received reply.
                     String[] res = received.split(":");
-                    if (res[0].equals("satelink.ip.ans")) {// si al hacer split del string
-                        // recivido el primer elemento es 'satelink.ip.ans' entonces el datagrama
-                        // proviene de satelink
+                    if (res[0].equals("satelink.ip.ans")) {// if split returns in [0] 'satelink.ip.ans' then is from satelink
                         running = false;
-                        this.estado = 2; // ya se ha encontrado la direccion ip del servidor
-                        //Log.i("+_+_+_+_+_+_+_", res[1]);
-                        // despues de hacer split al string de respuesta de satelink
-                        // su direccion ip queda en la segunda posicion del arreglo.
-                        DoWhenServerFound(res[1]);
+                        this.estado = 2; // server ip found
+                        r = res[1];
+                        //DoWhenServerFound(res[1]); // split in [1] is server ip
                     }
                 }
-                socket.close();
-            } catch (IOException e) {
+            } catch (IOException | NullPointerException e) {
+                reset();
                 e.printStackTrace();
+                setTXView_show_serverIP("Exepcion en udp broadcast");
             }
+            return r;
         }
 
-        public void reestablecer(){
-            this.estado=0;
-            try {
-                this.BROADCAST_IP = this.GetBroadcast();
-            } catch (SocketException | UnknownHostException e) {
-                e.printStackTrace();
-            }
-        }
 
         /**
          * cuando se encuentra el servidor se guarda la ip en preferencias y se muestra en
          * en los textview de la actividad.
          * @param ip
          */
-        public void DoWhenServerFound(final String ip){
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TXView_show_serverIP.setText(ip);
-                }
-            });
+        public void DoWhenComplete(final String ip){
+            edit.putString("host", ip);// se guarda la ip del servidor en preferencias.
+            edit.apply();
+            setTXView_show_serverIP(ip);
+            reset();
+        }
+
+        public void reset(){
+            this.estado=0;
+            this.running=false;
+            this.socket.close();
+            try {
+                this.broadcastIP = this.GetBroadcast();
+                this.socket = new DatagramSocket(12100);
+            } catch (SocketException | UnknownHostException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    public void setTXView_show_serverIP(final String txt){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TXView_show_serverIP.setText(txt);
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/rendidor/irsum/SuperAct.java
+++ app/src/main/java/com/rendidor/irsum/SuperAct.java
@@ -6,8 +6,7 @@ import android.widget.Toast;
 
 public class SuperAct extends AppCompatActivity {
     public void ShowT(String te) {
-        Toast to = Toast.makeText(getApplicationContext(), te, 1);
-        to.setDuration(Toast.LENGTH_SHORT);
+        Toast to = Toast.makeText(getApplicationContext(), te, Toast.LENGTH_SHORT);
         to.setGravity(17, 0, 0);
         to.show();
     }
